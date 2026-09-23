0f4f66dbd132ce9722f560bc14d6a58cbcb46149
Nicolas Manns <nicolas.manns@gmail.com>
2026-09-23 08:59:46 +0200

    chore: hide motorcycle sections

--- src/navigation.ts
+++ src/navigation.ts
@@ -4,7 +4,6 @@ import { getPermalink } from './utils/permalinks';
 export async function getHeaderData() {
   const quads = await getCollection('quad', ({ data }) => !data.draft);
   const leds = await getCollection('led', ({ data }) => !data.draft);
-  const motos = await getCollection('moto', ({ data }) => !data.draft);
 
   const ledLinks = leds
   .sort((a, b) =>
@@ -18,7 +17,7 @@ export async function getHeaderData() {
     image: item.data.image,
   }));
 
-  const promoLinks = [...quads, ...motos, ...leds]
+  const promoLinks = [...quads, ...leds]
   .filter((item) => item.data.promo)
   .sort((a, b) =>
     a.data.title.localeCompare(b.data.title, 'fr', {
@@ -30,23 +29,14 @@ export async function getHeaderData() {
     href:
       item.collection === 'quad'
         ? getPermalink(`/quads/${item.data.slug ?? item.id}`)
-        : item.collection === 'moto'
-          ? getPermalink(`/motos/${item.data.slug ?? item.id}`)
-          : getPermalink(`/leds/${item.data.slug ?? item.id}`),
+        : getPermalink(`/leds/${item.data.slug ?? item.id}`),
     image: item.data.image,
   }));
 
   return {
     links: [
       { text: 'Accueil', href: '/' },
-      {
-        text: 'Véhicules',
-        links: [
-          { text: 'Quads BENDA', href: '/quads',  image: '/media/quads/redstone-550-court/main.webp'},
-          { text: 'Motos BENDA', href: '/motos/benda', image: '/media/motos/benda-avengers-125-cc-–-custom-v-twin/avengers.png'},
-          { text: 'Motos MAG POWER', href: '/motos/mag-power', image: '/media/motos/magpower-bombers-50-cm³-e5/bombers-50-situation-6-ms-1024x768.jpg' },
-        ],
-      },
+      { text: 'Quads BENDA', href: '/quads' },
       {
         text: 'Leds & Accessoires',
         links: [
@@ -88,8 +78,6 @@ export const footerData = {
       title: 'Catalogue',
       links: [
         { text: 'Quads BENDA', href: '/quads' },
-        { text: 'Motos BENDA', href: '/motos/benda' },
-        { text: 'Motos MAG POWER', href: '/motos/mag-power' },
         { text: 'Leds & accessoires', href: '/leds' },
       ],
     },
